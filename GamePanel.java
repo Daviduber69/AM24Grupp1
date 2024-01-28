2d8d9e7170bd250b09e7d10cf92cefb92f0b471e
Daviduber69 <150329932+Daviduber69@users.noreply.github.com>
2024-01-28 13:36:07 +0100

    Added JPanel window and a rectangle (the bird)
    Mestadels ett test för att uppfriska mitt github-minne. Behöver ej vara detta vi använder...

---
+++ GamePanel.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    final int originalTileSize = 16; //16x16
+    final int scale = 3;
+    final int tileSize = originalTileSize * scale; //48x48 ((3*16)x(3x16))
+    final int maxScreenColumn = 12;
+    final int maxScreenRow = 16;
+    final int screenWidth = tileSize * maxScreenColumn; //48*12px
+    final int screenHeight = tileSize * maxScreenRow; //48*16 px
+
+    Thread gameThread;
+
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.blue);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while (gameThread != null) {
+            update();
+            repaint();
+        }
+    }
+
+    public void update() {
+
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(100,100, tileSize,tileSize);
+
+        g2.dispose();
+    }
+
+}
