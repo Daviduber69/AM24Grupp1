c6136528ad900bd12ae6a1290ac449b3e9737e0b
adamj0425 <adamj0425@gmail.com>
2024-01-31 13:49:18 +0100

    Changed title

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Flappy Birb");
+        window.setTitle("Jumpy Birb");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
