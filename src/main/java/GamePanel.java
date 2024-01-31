af2623e0c513ac44b0bd2e1c40f5bd1a76752e53
David Henriksson <davhen0108@skola.goteborg.se>
2024-01-31 13:02:00 +0100

    fixed pipes

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -7,8 +7,8 @@ public class GamePanel extends JPanel implements Runnable {
     final int tileSize = originalTileSize * scale; //16x3 = 48px
     final int maxScreenColumn = 12;
     final int maxScreenRow = 16;
-    final int screenWidth = tileSize * maxScreenColumn; //48*12px
-    final int screenHeight = tileSize * maxScreenRow; //48*16 px
+    final int screenWidth = tileSize * maxScreenColumn; //48*12px = 576 px
+    final int screenHeight = tileSize * maxScreenRow; //48*16 px = 768 px
 
     Thread gameThread;
 
@@ -40,11 +40,15 @@ public class GamePanel extends JPanel implements Runnable {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        Graphics2D g2 = (Graphics2D)g;
-
+        Graphics2D g2 = (Graphics2D) g;
+        Graphics2D pipe1 = (Graphics2D) g;
+        Graphics2D pipe2 = (Graphics2D) g;
         g2.setColor(Color.white);
-        g2.fillRect(100,100, tileSize,tileSize);
-
+        g2.fillRect(100, 100, tileSize, tileSize);
+        pipe1.setColor(Color.green);
+        pipe1.fillRect(510, 40, 50, 300);
+        pipe2.setColor(Color.green);
+        pipe2.fillRect(510, 500, 50, 300);
         g2.dispose();
     }
 
