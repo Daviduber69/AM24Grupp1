37f93886fbd0326319d6934c937addddd34006ec
David Henriksson <davhen0108@skola.goteborg.se>
2024-02-06 09:45:56 +0100

    fixed URL for image

--- src/main/java/ImagePanel.java
+++ src/main/java/ImagePanel.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class ImagePanel extends JPanel {
     public static Image image;
@@ -10,7 +11,8 @@ public class ImagePanel extends JPanel {
 
     public ImagePanel(){
         try{
-            image = ImageIO.read(new File("C:\\Users\\risif\\Desktop\\hamen.png"));
+          URL imageURL = new URL("https://github.com/Daviduber69/AM24Grupp1/blob/main/hamen.png?raw=true");
+            image = ImageIO.read(imageURL);
         }catch(IOException e){
             e.printStackTrace();
         }
