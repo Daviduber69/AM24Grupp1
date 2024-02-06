6bdc7f3a24332982fae20e78e9f38d290244b94f
David Henriksson <davhen0108@skola.goteborg.se>
2024-02-06 10:51:24 +0100

    fixed imagepanel

--- src/main/java/ImagePanel.java
+++ src/main/java/ImagePanel.java
@@ -7,22 +7,12 @@ import java.net.URL;
 
 public class ImagePanel extends JPanel {
     public static Image image;
-
-
-    public ImagePanel(){
-        try{
-          URL imageURL = new URL("https://github.com/Daviduber69/AM24Grupp1/blob/main/hamen.png?raw=true");
+    public ImagePanel() {
+        try {
+            URL imageURL = new URL("https://github.com/Daviduber69/AM24Grupp1/blob/main/hamen.png?raw=true");
             image = ImageIO.read(imageURL);
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-    public Dimension preferedSize(){
-        if(image!=null){
-            return new Dimension(image.getWidth(this),image.getHeight(this));
-        }
-        else {
-            return super.getPreferredSize();
         }
     }
 }
