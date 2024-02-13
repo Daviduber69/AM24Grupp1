410c00a9faa808fe36decc9ec10626089722393d
5ail3 <saile.dragednas@proton.me>
2024-02-13 10:19:34 +0100

    adding comments in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,17 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-            JFrame window = new JFrame();
-            GamePanel gamePanel = new GamePanel();
-            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            window.setResizable(false);
-            window.setTitle("Jumpy Birb");
-            window.add(gamePanel);
-            window.pack();
-            window.setLocationRelativeTo(null);
-            window.setVisible(true);
-            window.requestFocus();
-            gamePanel.startGameThread();
+            JFrame window = new JFrame();                           // Creates a new window
+            GamePanel gamePanel = new GamePanel();                  // Creates a new GamePanel
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Makes closing on demand available
+            window.setResizable(false);                   // Sets this frame not resizable by the user.
+            window.setTitle("Jumpy Birb");                    // Set title
+            window.add(gamePanel);                                  // Makes everything that is modified in gamePanel class visible in the window
+            window.pack();                                          // Layout estimation for the most preferable construction of the window
+            window.setLocationRelativeTo(null);                   // Center the window
+            window.setVisible(true);                              // Makes the window visable for the user
+            window.requestFocus();                                  // Allowing the user to interact with the window
+            // gameMenu.start()                                     // calling method gameMenu
+            gamePanel.startGameThread();                            // starting gameThread
         }
     }
