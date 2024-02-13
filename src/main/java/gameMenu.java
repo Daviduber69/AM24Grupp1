8b6795d03f1382524a6075b49805a4e4ba7674f9
5ail3 <saile.dragednas@proton.me>
2024-02-13 10:48:33 +0100

    adding information about the class gameMenu

--- src/main/java/gameMenu.java
+++ src/main/java/gameMenu.java
@@ -1,3 +1,15 @@
+    /*
+     * Jag vill skapa en klass som instansieras genom main-metoden.
+     * Denna klass kommer bli spelets meny.
+     * Här är det tänkt att man ska kunna välja svårighetsgrad, starta spelet samt se highscore.
+     * 
+     * Steg 1:
+     * Skapa ett objekt av gameMenu som ska kallas på genom main-klassen (eventuellt göra gameMenu till static,
+     * vad är skillnaden (?)).
+     * 
+     * Steg 2:
+     * 
+     */
 public class gameMenu {
-    
+
 }
