18d0d48bf9151a11267fb81b2c6d0ca3aacb236f
EliasSan <saile.dragednas@proton.me>
2024-03-18 15:50:49 +0100

    deleting comment above the GameMenu-class

--- src/main/java/GameMenu.java
+++ src/main/java/GameMenu.java
@@ -1,19 +1,3 @@
-    /*
-     * Jag vill skapa en klass som instansieras genom main-metoden.
-     * Denna klass kommer bli spelets meny.
-     * Här är det tänkt att man ska kunna välja svårighetsgrad, starta spelet samt se highscore.
-     * 
-     * Steg 1:
-     * Skapa ett objekt av gameMenu som ska kallas på genom main-klassen (eventuellt göra gameMenu till static,
-     * vad är skillnaden (?)).
-     * 
-     * Steg 2:
-     * Skapa funktion för att starta spelet.
-     * Denna funktion ska reagerar på input från spaceknappen (ev. vänster musklick)
-     * 
-     * 
-     */
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
